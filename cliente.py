a3b7885fdd392c6a604ee27206b89822a07cb3f6
MarinaDaumas <marina.daumas@poli.ufrj.br>
2022-01-26 00:23:24 -0300

    cliente e server prontos

--- cliente.py
+++ cliente.py
@@ -1,80 +1,53 @@
 from socket import *
 
-serverName = "127.0.0.1" #endereço de IP do servidor
-serverPort = 12000 # numero da porta que o servidor está
+serverName = "127.0.0.1" 
+serverPort = 12000 
 
-#indica o processo que esta rodado no outro hospedeiro?
-clientSocket = socket(AF_INET, SOCK_STREAM) #aqui mostra que é TCP, criação do socket de cliente
-
-clientSocket.connect((serverName, serverPort)) #cria o cliente e conecta ao servidor e a porta.
+clientSocket = socket(AF_INET, SOCK_STREAM) 
+clientSocket.connect((serverName, serverPort)) 
+clientSocket.settimeout(2)
 
 # teste de conexão
-connection_test = "connected".encode()
-clientSocket.send(connection_test)
+clientSocket.send("connected".encode())
 conectado = False
 try: 
 	clientSocket.recv(1024)
 	conectado = True
 except:
 	print("Erro de conexão.")
-	clientSocket.close()#testou se conecttou
+	clientSocket.close()
+
 
 if conectado:
-	matricula = input("Input matricula:").encode() #escreve a matricula, input
-
-	# codifica a mensagem
+	matricula = input("Input matricula:").encode() 
 	
 	received = False
-	while not received: #só sai do loop depois que recebe
-		clientSocket.send(matricula) # matricula enviada
+	while not received: 
+		clientSocket.send(matricula) # envia a matricula até que seja recebida corretamete
 		try:
-			clientSocket.settimeout(10) #espera no maximo 10 segundos para a resposta
-			ack_0 = clientSocket.recv(1024)#recebe o codigo enviado da matricula
+			ack_0 = clientSocket.recv(1024)
 			if (ack_0 == matricula):
-				clientSocket.send("ok".encode())#se for igual, manda ok
+				clientSocket.send("ok".encode()) # confirma que a matrícula foi recebida
 				received = True
 			else:
 				received = False
 
 		except:
 			received = False
+    
+	clientSocket.settimeout(10) # define timeout maior para dar tempo de receber as notas caso haja erro no envio
 
-	"""
-	matricula = int(connectionSocket.recv(1024).decode('UTF-8')) #pega o que vem do socket
-	connectionSocket.send(matricula)
-	x = connectionSocket.recv(1024).decode('UTF-8')
-
-	while x != "ok":
-		matricula = x
-		connectionSocket.send(matricula)
-		x = connectionSocket.recv(1024).decode('UTF-8')
-
-
-	while x == "not ok":
-		x = notas
-		notas = clientSocket.recv(1024)
-		connectionSocket.send(notas)
-		x = connectionSocket.recv(1024).decode('UTF-8')
-
-	while x != "ok":
-		matricula = x
-		connectionSocket.send(matricula)
-		x = connectionSocket.recv(1024).decode('UTF-8')
-
-
-
-	received = False
-	while not received:
-		try: 
-			clientSocket.settimeout(10)
-			notas = clientSocket.recv(1024)
-			clientSocket.send(notas)
-
-		except:
-			pass
-    """
 	notas = clientSocket.recv(1024)
+	clientSocket.send(notas)
+	x = clientSocket.recv(1024)
 
-	print ("From Server:", notas.decode('UTF-8'))
+	while x.decode('UTF-8') != "ok": 
+		notas = x
+		clientSocket.send(notas)
+		x = clientSocket.recv(1024)
+
+	notas = notas.decode('UTF-8')
+
+	print ("From Server:", notas)
 	
-	clientSocket.close()
+	#clientSocket.close()
