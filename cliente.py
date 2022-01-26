7d5039bdc96c097be2511dfc72bb2d4af009a94d
MarinaDaumas <marina.daumas@poli.ufrj.br>
2022-01-25 22:54:03 -0300

    "mudanças em grupo"

--- cliente.py
+++ cliente.py
@@ -1,12 +1,80 @@
 from socket import *
-serverName = "127.0.0.1"
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentence = input("Input matricula:").encode()
-clientSocket.send(sentence)
 
-nota1 = clientSocket.recv(1024)
-nota2 = clientSocket.recv(1024)
-print ("From Server:", nota1.decode('UTF-8'), " ", nota2.decode('UTF-8'))
-clientSocket.close()
+serverName = "127.0.0.1" #endereço de IP do servidor
+serverPort = 12000 # numero da porta que o servidor está
+
+#indica o processo que esta rodado no outro hospedeiro?
+clientSocket = socket(AF_INET, SOCK_STREAM) #aqui mostra que é TCP, criação do socket de cliente
+
+clientSocket.connect((serverName, serverPort)) #cria o cliente e conecta ao servidor e a porta.
+
+# teste de conexão
+connection_test = "connected".encode()
+clientSocket.send(connection_test)
+conectado = False
+try: 
+	clientSocket.recv(1024)
+	conectado = True
+except:
+	print("Erro de conexão.")
+	clientSocket.close()#testou se conecttou
+
+if conectado:
+	matricula = input("Input matricula:").encode() #escreve a matricula, input
+
+	# codifica a mensagem
+	
+	received = False
+	while not received: #só sai do loop depois que recebe
+		clientSocket.send(matricula) # matricula enviada
+		try:
+			clientSocket.settimeout(10) #espera no maximo 10 segundos para a resposta
+			ack_0 = clientSocket.recv(1024)#recebe o codigo enviado da matricula
+			if (ack_0 == matricula):
+				clientSocket.send("ok".encode())#se for igual, manda ok
+				received = True
+			else:
+				received = False
+
+		except:
+			received = False
+
+	"""
+	matricula = int(connectionSocket.recv(1024).decode('UTF-8')) #pega o que vem do socket
+	connectionSocket.send(matricula)
+	x = connectionSocket.recv(1024).decode('UTF-8')
+
+	while x != "ok":
+		matricula = x
+		connectionSocket.send(matricula)
+		x = connectionSocket.recv(1024).decode('UTF-8')
+
+
+	while x == "not ok":
+		x = notas
+		notas = clientSocket.recv(1024)
+		connectionSocket.send(notas)
+		x = connectionSocket.recv(1024).decode('UTF-8')
+
+	while x != "ok":
+		matricula = x
+		connectionSocket.send(matricula)
+		x = connectionSocket.recv(1024).decode('UTF-8')
+
+
+
+	received = False
+	while not received:
+		try: 
+			clientSocket.settimeout(10)
+			notas = clientSocket.recv(1024)
+			clientSocket.send(notas)
+
+		except:
+			pass
+    """
+	notas = clientSocket.recv(1024)
+
+	print ("From Server:", notas.decode('UTF-8'))
+	
+	clientSocket.close()
--- server.py
+++ server.py
@@ -1,19 +1,45 @@
 from socket import *
 
-notas = {119050872: {"T1": 8.8, "T2": 9.0}, 119048468: {"T1": 8.2, "T2": 9.1}, 112948927: {"T1": 8.8, "T2": 9.0}, 118048725: {"T1": 8.8, "T2": 9.0}}
+notas = {1: {"T1": 8.8, "T2": 9.0}, 2: {"T1": 8.2, "T2": 9.1}, 3: {"T1": 8.5, "T2": 9.2}, 4: {"T1": 7, "T2": 9.0} ,5: {"T1": 5, "T2": 7}, 6: {"T1": 7, "T2": 9}, 7: {"T1": 8.7, "T2": 9.0}, 8: {"T1": 8.8, "T2": 9.0}, 9: {"T1": 5.7, "T2": 9.0}, 10: {"T1": 8.8, "T2": 9.0},11: {"T1": 7.7, "T2": 9.0},
+12: {"T1": 8.8, "T2": 8}, 13: {"T1": 10, "T2": 9.1}, 14: {"T1": 5, "T2": 6}, 15: {"T1": 7, "T2": 9.0} ,16: {"T1": 8.8, "T2": 9.9}, 17: {"T1": 8.7, "T2": 9.3}, 18: {"T1": 7, "T2": 8}, 19: {"T1": 8.8, "T2": 7}, 20: {"T1": 7, "T2": 7}, 21: {"T1": 6.8, "T2": 9.0},22: {"T1": 7.8, "T2": 9.0},  }
 
 
-serverPort = 12000
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(("127.0.0.1",serverPort))
-serverSocket.listen(1)
-print ("The server is ready to receive")
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    matricula = int(connectionSocket.recv(1024).decode('UTF-8'))
-    print(matricula)
-    t1, t2 = "T1: " + str(notas[matricula]["T1"]), "T2: " + str(notas[matricula]["T2"])
-    connectionSocket.send(t1.encode())
-    connectionSocket.send(t2.encode())
+serverPort = 12000 #porta 
+serverSocket = socket(AF_INET,SOCK_STREAM) # criação do socket, stream: fica ouvindo continuamente
+serverSocket.bind(("127.0.0.1",serverPort)) # se conecta a porta
+serverSocket.listen(1) # ouve de 1 em 1 segundo
+# serverSocket.settimeout()
 
-    connectionSocket.close()
+print ("The server is ready to receive") #welcome socket?
+
+connectionSocket, addr = serverSocket.accept()
+pkt_0 = connectionSocket.recv(1024)
+connectionSocket.send(pkt_0)
+    
+while True:      
+    matricula = connectionSocket.recv(1024) #pega o que vem do socket
+    connectionSocket.send(matricula)
+    x = connectionSocket.recv(1024)
+
+    while x.decode('UTF-8') != "ok":
+        matricula = x
+        connectionSocket.send(matricula)
+        x = connectionSocket.recv(1024)
+
+    matricula = int(matricula.decode('UTF-8'))
+
+    try:
+        resultado_notas = "Nota T1: " + str(notas[matricula]["T1"]) + " Nota T2: " + str(notas[matricula]["T2"]) #le no dicionario se essa matricula está lá e pega
+
+    except:
+        resultado_notas = "Matricula não cadastrada"
+        
+    connectionSocket.send(resultado_notas.encode()) #retorna a mudanca
+    
+    # else:
+    #     matricula = int(connectionSocket.recv(1024).decode('UTF-8')) #pega o que vem do socket
+    #     connectionSocket.send(matricula)
+
+connectionSocket.close() #fecha 
+
+    
